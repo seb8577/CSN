267deb3b54a8843d41c8af4d32e04574e7543746
seb <sebteill@gmail.com>
2019-12-05 15:16:20 +0100

    ajout express&handlebars

---
+++ app.js
@@ -0,0 +1,24 @@
+const express   = require('express');
+const exphbs    = require('express-handlebars');
+
+const app = express();
+
+
+
+// ROUTE
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get("/", function (req, res) {
+    res.render("index")
+})
+
+app.get("/entreprise", function(req, res) {
+    res.render("entreprise")
+})
+
+
+
+app.listen(3000, function() {
+    console.log("le serveur tourne sur le port 3000");
+})
