9cf57cdf7848e9471412eb33cf28e3c527c5a271
seb <sebteill@gmail.com>
2019-12-05 15:50:45 +0100

    ajout pages

--- app.js
+++ app.js
@@ -8,20 +8,37 @@ app.use(express.static('public'));
 
 
 
-// ROUTE
+// ROUTES
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/entreprise", function(req, res) {
+app.get("/entreprise", (req, res) => {
     res.render("entreprise")
+})
+
+app.get("/realisations", (req, res) => {
+    res.render("realisations")
+})
+
+app.get("/devis", (req, res) => {
+    res.render("devis")
+})
+
+app.get("/ftechniques", (req, res) => {
+    res.render("ftechniques")
+})
+
+app.get("/actualites", (req, res) => {
+    res.render("actualites")
 })
 
 
 
+// SERVEUR LOCAL
 app.listen(3000, function() {
     console.log("le serveur tourne sur le port 3000");
 })
